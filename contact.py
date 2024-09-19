4e8ded8cff18709949fd9cc5b78f7503e22d3bc6
Alex Monge <monge.alex572@gmail.com>
2024-09-19 13:15:27 -0500

    1

--- contact.py
+++ contact.py
@@ -55,4 +55,5 @@ def send_email():
         return jsonify({'status': 'error', 'message': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
